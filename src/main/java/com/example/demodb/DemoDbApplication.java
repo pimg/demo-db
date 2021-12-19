c328747db70cd392dfc69b5bcbc1239e5f90edd2
pimg <pim.gaemers@gmail.com>
2021-12-19 14:21:33 +0100

    extracted query to separe method

--- src/main/java/com/example/demodb/DemoDbApplication.java
+++ src/main/java/com/example/demodb/DemoDbApplication.java
@@ -11,5 +11,4 @@ public class DemoDbApplication {
 		SpringApplication.run(DemoDbApplication.class, args);
 	}
 
-
 }
--- src/main/java/com/example/demodb/repositories/impl/BookRepositoryImpl.java
+++ src/main/java/com/example/demodb/repositories/impl/BookRepositoryImpl.java
@@ -5,7 +5,6 @@ import com.example.demodb.repositories.BookRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
-
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
@@ -22,32 +21,32 @@ public class BookRepositoryImpl implements BookRepository {
 
 	@Override
 	public Mono<Book> findByIsbn(String isbn) {
-		CriteriaBuilder criteriaBuilder = sessionFactory.getCriteriaBuilder();
-		CriteriaQuery<Book> query = criteriaBuilder.createQuery(Book.class);
-
-		Root<Book> root = query.from(Book.class);
-		query.where(criteriaBuilder.equal(root.get("isbn"), isbn));
-
 		return sessionFactory.withSession(session -> session
-			.createQuery(query)
-			.getSingleResult()
-			.onItem()
-			.call(book -> fetch(book.getPages())))
-			.convert().with(toMono());
+				.createQuery(buildFindBookByIsbnQuery(isbn))
+				.getSingleResult()
+				.onItem()
+				.call(book -> fetch(book.getPages()))
+			)
+			.convert()
+			.with(toMono());
 	}
 
 	@Override
 	public Mono<Book> findSummaryByIsbn(String isbn) {
+		return sessionFactory.withSession(session -> session
+				.createQuery(buildFindBookByIsbnQuery(isbn))
+				.getSingleResult()
+			)
+			.convert()
+			.with(toMono());
+	}
+
+	private CriteriaQuery<Book> buildFindBookByIsbnQuery(String isbn) {
 		CriteriaBuilder criteriaBuilder = sessionFactory.getCriteriaBuilder();
 		CriteriaQuery<Book> query = criteriaBuilder.createQuery(Book.class);
 
 		Root<Book> root = query.from(Book.class);
 		query.where(criteriaBuilder.equal(root.get("isbn"), isbn));
-
-		return sessionFactory.withSession(session -> session
-				.createQuery(query)
-				.getSingleResult()
-			)
-			.convert().with(toMono());
+		return query;
 	}
 }
